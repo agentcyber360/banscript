73f5005b2c0a8ca59416ffe37f55fe975af86d75
Peter Iwanowitsch <peter@iwanowitsch.net>
2022-06-16 09:24:50 +0200

    Refactoring if condition

--- ban.sh
+++ ban.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-if [[ -z "$1" ]]; then
+if [[ -z "$1" ]]
+then
   echo "[!] Error: Please use syntax: ban.sh [IP]"
-exit 0
+  exit 0
+else
+  iptables -I INPUT -s $1 -j DROP
+  echo "SUCCESS: Banned IP $1"
+  exit 0
 fi
-iptables -I INPUT -s $1 -j DROP
-echo "Banned IP: $1"
-exit 0
+
